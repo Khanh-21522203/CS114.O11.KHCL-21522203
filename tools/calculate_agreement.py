15cf875dff1dabb17bfa9703101115f68d5b5dee
bmhungqb <21522110@gm.uit.edu.vn>
2024-01-11 01:06:06 +0700

    add the measure of agreement Krippendorff's alpha

--- tools/calculate_agreement.py
+++ tools/calculate_agreement.py
@@ -1,7 +1,10 @@
 import os.path
 import numpy as np
 import pandas as pd
-fname = ['data_dsmlvietnam.csv', 'data_mlcoban.csv'] # 647 -
+from sklearn.metrics import cohen_kappa_score
+from nltk.metrics.agreement import AnnotationTask
+from nltk.metrics import masi_distance
+fname = ['data_dsmlvietnam.csv', 'data_mlcoban.csv']
 list_hashtag = ["#sharing", "#machine_learning", "#deep_learning", "#python", "#data", "#cv", "#nlp", "#math", "#Q&A", "#webinar"]
 Hung = []
 Khanh = []
@@ -14,20 +17,75 @@ for fn in fname:
             it_Khanh = []
             it_Cap = []
             for i in row["Hùng"].split(','):
+                i = i.strip()
+                if i == "Q&A":
+                    i = "#Q&A"
+                elif i == "#machine_learing":
+                    i = "#machine_learning"
+                elif i == "#deep_leaning":
+                    i = "#deep_learning"
+                elif i == "#deep_learing":
+                    i = "#deep_learning"
+                elif i == "#webniar":
+                    i = "#webinar"
+                elif i == "#pythonlibararies":
+                    i = "#pythonlibraries"
                 if i == "#pythonlibraries":
                     i = "#python"
                 if i in list_hashtag:
                     it_Hung.append(i)
-            Hung.append(it_Hung)
             for i in row["Khánh"].split(','):
+                i = i.strip()
+                if i == "Q&A":
+                    i = "#Q&A"
+                elif i == "#machine_learing":
+                    i = "#machine_learning"
+                elif i == "#deep_leaning":
+                    i = "#deep_learning"
+                elif i == "#deep_learing":
+                    i = "#deep_learning"
+                elif i == "#webniar":
+                    i = "#webinar"
+                elif i == "#pythonlibararies":
+                    i = "#pythonlibraries"
                 if i == "#pythonlibraries":
                     i = "#python"
                 if i in list_hashtag:
                     it_Khanh.append(i)
-            Khanh.append(it_Khanh)
             for i in row["Cáp"].split(','):
+                i = i.strip()
+                if i == "Q&A":
+                    i = "#Q&A"
+                elif i == "#machine_learing":
+                    i = "#machine_learning"
+                elif i == "#deep_leaning":
+                    i = "#deep_learning"
+                elif i == "#deep_learing":
+                    i = "#deep_learning"
+                elif i == "#webniar":
+                    i = "#webinar"
+                elif i == "#pythonlibararies":
+                    i = "#pythonlibraries"
                 if i == "#pythonlibraries":
                     i = "#python"
                 if i in list_hashtag:
                     it_Cap.append(i)
-            Cap.append(it_Cap)
+            if len(it_Cap) and len(it_Hung) and len(it_Khanh):
+                Cap.append(it_Cap)
+                Hung.append(it_Hung)
+                Khanh.append(it_Khanh)
+task_data = []
+print("length of data: ",len(Hung))
+for i in range(len(Hung)):
+    item_H = ('Hung', str(i), frozenset(Hung[i]))
+    item_K = ('Khanh', str(i), frozenset(Khanh[i]))
+    item_C = ('Cap', str(i), frozenset(Cap[i]))
+    task_data.append(item_H)
+    task_data.append(item_K)
+    task_data.append(item_C)
+
+task = AnnotationTask(distance = masi_distance)
+
+task.load_array(task_data)
+
+print(task.alpha())
