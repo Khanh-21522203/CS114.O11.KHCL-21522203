16d6ccde1ddf03cf120875799109bd02e600ab7c
Khanh-21522203 <21522203@gm.uit.edu.vn>
2024-01-12 21:39:17 +0700

    thay đổi tiền xử lý dữ liệu

--- tools/preprocessing.py
+++ tools/preprocessing.py
@@ -1,64 +1,44 @@
-import requests
-from underthesea import word_tokenize
 import re
-from urlextract import URLExtract
-API_URL = "https://api-inference.huggingface.co/models/bmd1905/vietnamese-correction"
-headers = {"Authorization": "Bearer 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"}
+from pyvi import ViTokenizer
 
-def spell_correction(text):
-	payload = {"inputs": text}
-	response = requests.post(API_URL, headers=headers, json=payload)
-	response = response.json()
+def normalize(text):
+    t = text.replace('\n', ' ')
+    t = t.lower()
+    return t
 
-	output = response[0]['generated_text']
-    
-	return output
+def delete_hashtag(text):
+    return re.sub(r'#\w+', '', text)
 
-def lowercase(text):
-    return text.lower()
+def delete_link(text):
+    return re.sub(r'http\S+', '', text)
 
-def remove_url(text):
-    extractor = URLExtract()
-    url_to_remove = extractor.find_urls(text)
-    for word in url_to_remove:
-        text = re.sub(r'\b' + re.escape(word) + r'\b', " ", text, flags=re.IGNORECASE)
-    return text
+def remove_emojis(text):
+    emoj = re.compile(r"""[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF\U00002702-\U000027B0\U000024C2-\U0001F251\U0001f926-\U0001f937\U00010000-\U0010ffff\u200d\u23cf\u23e9\u231a\ufe0f\u3030-]+(?<!\n)""", re.UNICODE)
+    return re.sub(emoj, '', text)
 
-def remove_emojis(data):
-  emoj = re.compile(r"""[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF\U00002702-\U000027B0\U000024C2-\U0001F251\U0001f926-\U0001f937\U00010000-\U0010ffff\u200d\u23cf\u23e9\u231a\ufe0f\u3030-]+(?<!\n)""", re.UNICODE)
-  return re.sub(emoj, '', data)
+def encode_number(text):
+    t = text.split(' ')
+    t = map(lambda x: '<number>' if bool(re.match(r'^[0-9]+(\.[0-9]+)?$', x)) else x, t)
+    return ' '.join(t)
 
-def word_segmentation(text):
-    return word_tokenize(text)
-    
-def denoise(text):
-	text = re.sub(r'[^\s\wáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđ_]',' ',text)
-	text = re.sub(r'\s+', ' ', text).strip()
-	return text
-def stopwords():
-    with open('vietnamese-stopwords.txt', 'r') as f:
-        vietnamese_stopwords = f.read().splitlines()
-    return vietnamese_stopwords
-    
-def remove_stopword(text):
-    with open('data/vietnamese-stopwords.txt', 'r', encoding='utf-8') as f:
-        vietnamese_stopwords = f.read().splitlines()
-    for word in vietnamese_stopwords:
-        text = re.sub(r'\b' + re.escape(word) + r'\b', "", text, flags=re.IGNORECASE)
-    return text
-
+def delete_onelen_token(text):
+    t = text.split(' ')
+    t = filter(lambda x: len(x)>1, t)
+    return ' '.join(t)
 
 def preprocessing(text):
-    
-    text = remove_emojis(text)
-    text = remove_url(text)
-    text = denoise(text)
-    # text = spell_correction(text)
-    text = lowercase(text)
-    text = remove_stopword(text)
-    text = word_segmentation(text)
-    return text
+    t = normalize(text)
+    t = delete_hashtag(t)
+    t = delete_link(t)
+    t = remove_emojis(t)
+    t = ViTokenizer.tokenize(t)
+    t = encode_number(t)
+    t = delete_onelen_token(t)
+    return t
+
 if __name__=="__main__":
-    # print(spell_correction("Lần này anh Phươngqyết xếp hàng mua bằng được 1 chiếc"))
-    # print(word_segmentation("Bác sĩ bây giờ có thể thản nhiên báo tin bệnh nhân bị ung thư"))
-    print(denoise("repo +  !@ !@$#@^%$%^(^&) /sa 2jijf bacij"))
+    text = """dạ chào anh Việt và mọi người, hiện tại em đang làm đồ án dự báo công suất phát nhà máy điện mặt trời bằng mô hình LSTM.
+            Bộ dữ liệu em đang có là bức xạ, nhiệt độ môi trường, nhiệt độ tấm pin, và công suất phát của nhà máy.
+            Em học điện, cũng mới tìm hiểu mảng này, nên mọi người ai có làm rồi có ý tưởng hay tài liệu gì chia sẻ giúp em với ạ.
+            em cám ơn mọi người."""
+    print(preprocessing(text))
